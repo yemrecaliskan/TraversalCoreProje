a5fb7c0cda355ef2c2cb07a63749a8f349381d8e
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-04-29 15:27:18 +0300

    Layout Düzenlemeleri ve Tur Rotaları Detayları

--- DataAccessLayer/Migrations/20220429121609_mig_add_destination_Details.Designer.cs
+++ DataAccessLayer/Migrations/20220429121609_mig_add_destination_Details.Designer.cs
@@ -3,14 +3,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220429121609_mig_add_destination_Details")]
+    partial class mig_add_destination_Details
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
