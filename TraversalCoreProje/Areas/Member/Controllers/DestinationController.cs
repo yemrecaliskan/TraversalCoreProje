7362f59554792bb0fe374a5e9ae1c2d17b5c3b24
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-06-13 14:32:02 +0300

    Member Sayfasında Verilerin Listelenmesi

---
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager dm = new(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = dm.TGetList();
+            return View(values);
+        }
+    }
+}
