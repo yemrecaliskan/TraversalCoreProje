b92430b28a36498b5876e7fc3bcff72a1395ed6c
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-06-13 13:26:39 +0300

    Custom Identity Validator

---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length) => 
+            new IdentityError { 
+                Code = "PasswordTooShort", 
+                Description = $"Parola en az {length} karakter olmalıdır." 
+            };
+
+        public override IdentityError PasswordRequiresUpper() =>
+            new IdentityError
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Parola en az 1 büyük harf içermelidir."
+            };
+
+        public override IdentityError PasswordRequiresLower() =>
+            new IdentityError
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parola en az 1 küçük harf içermelidir."
+            };
+
+        public override IdentityError PasswordRequiresNonAlphanumeric() =>
+            new IdentityError
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola en az bir alfanumerik olmayan karaktere sahip olmalıdır."
+            };
+
+        public override IdentityError PasswordRequiresDigit() =>
+            new IdentityError
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Parola en az bir rakamdan ('0'-'9') oluşmalıdır."
+            };
+    }
+}
