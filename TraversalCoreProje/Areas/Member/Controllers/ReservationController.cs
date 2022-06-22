8aa14ac9f6edd580fa3b4680eb5f1eb46fafe2c5
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-06-22 14:18:21 +0300

    Rezervasyon Sayfası

---
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.ListCity = values;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            p.AppUserId = 3;
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
+        }
+    }
+}
